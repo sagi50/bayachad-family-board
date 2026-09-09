8191f68bab9edecfa792978337e8aa41b97f14aa
sagi50 <sagi_94@hotmail.com>
2026-09-09 09:00:03 +0300

    Link shopping list from home

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,8 @@
-import Board from './board'; export default function Home() { return <Board />; }
+import Board from './board';
+
+export default function Home() {
+  return <>
+    <a href="/shopping" className="shopping-fab" aria-label="פתיחת רשימת הקניות">רשימת קניות</a>
+    <Board />
+  </>;
+}
